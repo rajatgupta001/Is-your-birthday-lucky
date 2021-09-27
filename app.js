487015b3b7fb3749582be338f639bd3775d02aa8
Rajat Gupta <dguptarajat@gmail.com>
2021-09-27 19:27:20 +0530

    removed styles.css as there was some problem.

--- app.js
+++ app.js
@@ -6,29 +6,31 @@ const resultNegative = document.querySelector(".result-negative");
 
 function sumOfDigits(dob)
 {
+    var sum = 0;
     dob = dob.replaceAll("-", "");
     for(var i = 0; i < dob.length; i++)
     {
-        var sum = 0;
         sum = sum + Number(dob.charAt(i));
-        return sum;
     }
-}
+    return sum;
+};
 
 function luckyOrNot(sum){
-    if(sum % luckyNumber.value === 0){
+    if(sum % Number(luckyNumber.value) === 0){
         resultNegative.style.display = "none";
-        resultPositive.innerText = "Congrats! your birthday is LUCKY";
+        resultPositive.innerText = "Congrats! your birthday is LUCKY!!!";
+        resultPositive.style.display = "block";
     }
     else{
         resultPositive.style.display = "none";
-        resultNegative.innerText = "Your birthday is not lucky, so sad";
+        resultNegative.innerText = "Your birthday is not lucky, but don't be sad.....go and make your own luck.";
+        resultNegative.style.display = "block";
     }
-}
+};
 
 function clickEventHandler(){
     var sum = sumOfDigits(dob.value);
     luckyOrNot(sum);
-}
+};
 
 btnCheck.addEventListener("click", clickEventHandler);
